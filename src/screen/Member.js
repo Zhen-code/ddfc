4da47b85a5c300c2713912950a0d42998907bba3
六月的雨 <1468061103@qq.com>
2019-12-06 15:47:54 +0800

    "修改嘟嘟会员列表样式，会员列表数据为空"

--- src/screen/Member.js
+++ src/screen/Member.js
@@ -1,79 +1,71 @@
 import React from 'react';
 import Header from "../components/common/Header"
-import style from "../styles/member.css"
-export default class Home extends React.PureComponent {
+import style from "../styles/member.module.css"
+import storage from "../util/setStorage";
+export default class Member extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
             ordinary_users:[],
             team_manager:[],
-            team_director:[]
+            team_director:[],
+            left: 6,
+            pageIndex: 1,
+            pageSize: 12
         }
+        this.token=storage.getItem('token')
     }
-
+    getMember=(id)=>{
+        window.show();
+        const {pageIndex,pageSize}=this.state;
+        window.axios({
+            url:window.API.Inviter.iniviter_list+`?pageIndex=${pageIndex}&pageSize=${pageSize}`,
+            method: 'GET',
+            headers:{
+                'Authorization': this.token
+            }
+        }).then(res=>{
+               if(res.code===200){
+                   if(res.data.list.length===0){
+                       window.showToast('暂无数据')
+                   }else{
+                       window.hide();
+                   }
+               }
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
     componentDidMount() {
 
     }
-    componentWillUpdate(nextProps, nextState, nextContext) {
-
-    }
-    move1(){
-        let active=document.getElementById("active")
-        active.style.left="10px"
-    }
-    move2(){
-        let active=document.getElementById("active")
-        active.style.left="140px"
-    }
-    move3(){
-        let active=document.getElementById("active")
-        active.style.left="260px"
-    }
 
     render() {
-
         return (
             <div>
                 <Header/>
                 <ul className={style.role_list}>
-                    <li onClick={this.move1}>普通用户</li>
-                    <span>|</span>
-                    <li onClick={this.move2}>团队经理</li>
-                    <span>|</span>
-                    <li onClick={this.move3}>团队总监</li>
-                    <div className={style.active} id={"active"}></div>
+                    <li onClick={()=>{this.setState({left:6},()=>{this.getMember(1)})}}>普通用户</li>
+                    <span></span>
+                    <li onClick={()=>{this.setState({left:128},()=>{this.getMember(2)})}}>团队经理</li>
+                    <span></span>
+                    <li onClick={()=>{this.setState({left:246},()=>{this.getMember(3)})}}>团队总监</li>
+                    <div className={style.line} style={{left:this.state.left+'px'}}></div>
                 </ul>
                 <div className={style.recommend}>
-                    <span>直推人数:<label className={style.number}>3</label>人</span>
-                    <span>间推人数:<label className={style.number}>12</label>人</span>
+                    <div>直推人数：<span>3</span>人</div>
+                    <div>间推人数：<span>12</span>人</div>
                 </div>
-                <div className={style.recommend_list}>
-                    <p>
-                        <span>推荐类型:<span>直接</span></span>
-                        <span className={style.time}>
-                            2019-10-12 18:56:44
-                        </span>
-                    </p>
-                    <p>用户信息:<span className={style.info}>欧阳 139****4472</span></p>
-                </div>
-                <div className={style.recommend_list}>
-                    <p>
-                        <span>推荐类型:<span>直接</span></span>
-                        <span className={style.time}>
-                            2019-10-12 18:56:44
-                        </span>
-                    </p>
-                    <p>用户信息:<span className={style.info}>欧阳 139****4472</span></p>
-                </div>
-                <div className={style.recommend_list}>
-                    <p>
-                        <span>推荐类型:<span>直接</span></span>
-                        <span className={style.time}>
-                            2019-10-12 18:56:44
-                        </span>
-                    </p>
-                    <p>用户信息:<span className={style.info}>欧阳 139****4472</span></p>
-                </div>
+                <ul className={style.recommend_List}>
+                    <li className={style.item}>
+        `           <div>
+                        <span>推荐类型：直推</span>
+                        <span>2019-10-12 18:56:44</span>
+                    </div>
+                        <div>用户信息：欧阳  13954474472</div>
+                    </li>
+                    <li></li>
+                </ul>
             </div>
         )
     }
