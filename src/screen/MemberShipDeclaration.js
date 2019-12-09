5d6dcbc5a32cf017b91c7ba94b395e1ab64bfcbf
六月的雨 <1468061103@qq.com>
2019-12-09 19:35:05 +0800

    "完成订单列表显示，邀请人绑定，查看已选套餐(未进行订单数量与价格统计)"

--- src/screen/MemberShipDeclaration.js
+++ src/screen/MemberShipDeclaration.js
@@ -1,54 +1,133 @@
 import React from 'react';
+import {Fragment} from 'react';
 import Header from "../components/common/Header"
-import style from "../styles/membershipdeclaration.css"
+import style from "../styles/membershipdeclaration.module.css"
 import wcpay from "../assets/image/wcpay.png";
 import icon_cs from "../assets/image/icon_cs.png";
+import storage from "../util/setStorage";
 export default class MemberShipDeclaration extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
+            name: '',
+            phone: '',
+            inviteName: '',
+            checked: false,
+            orderList: [],
+            crowdfundingPrice: 0,
+            partPrice: 0,
+            countAll: 0
+        }
+        this.token=storage.getItem('token')
+        this.addInvite=this.addInvite.bind(this)
+        this.goPay=this.goPay.bind(this)
+    }
+    addInvite(){
+        const {name,phone,inviteName} =this.state
+        if(name===''||inviteName===''){
+            window.showToast('姓名不能为空!')
+            return
+        }else if(phone.length!=11){
+            window.showToast('请输入正确的手机号码!')
+            return
+        }
+        window.axios({
+            url:window.API.Inviter.member+'?inviterPhone='+phone+'&realName='+inviteName,
+            method:'POST',
+            headers:{
+                'Authorization':this.token
+            }
+        }).then(res=>{
+            window.showToast(res.msg)
+        }).catch(err=>{
+                console.log(err)
+        })
+    }
+    change(e){
+        this.setState({
+            checked:  e.currentTarget.checked
+        })
+    }
+    goPay(){
+        const {checked} =this.state;
+        if(checked){
+            window.axios({
 
+            })
+        }else{
+            window.showToast('请先阅读并同意协议')
         }
     }
     componentDidMount() {
-
+            window.axios({
+                url: window.API.Crowd_funding.order_list+'?pageIndex='+1+'&pageSize='+12,
+                method: 'GET',
+                headers: {
+                    'Authorization': this.token
+                }
+            }).then(res=>{
+                   const newList=res.data.list.filter(function (item) {
+                        if(item.status==1){
+                            return item;
+                        }
+                   })
+                this.setState({
+                    orderList:newList,
+                    countAll: newList.length
+                })
+                console.log(newList)
+            }).catch(err=>{
+                console.log(err)
+            })
     }
 
     render() {
+        const {orderList,crowdfundingPrice,partPrice,countAll}=this.state
         return (
-            <div style={{backgroundColor:'#EDEDED'}}>
+            <Fragment>
                 <Header/>
                 <div className={style.container}>
                     <div className={style.info}>
-                        <p><label>*</label>真实姓名<input type={'text'}/></p>
-                        <p><label>*</label>手机号<input type={'text'}/></p>
-                        <p><label>*</label>邀请人手机号<input type={'text'}/></p>
+                        <div><p><span>*</span><label>真实姓名</label></p><input type={'text'} onChange={(e)=>{this.setState({name:e.target.value})}}/></div>
+                        <div><p><span>*</span><label>手机号</label></p><input type={'text'} onChange={(e)=>{this.setState({phone:e.target.value})}}/></div>
+                        <div><p><span>*</span><label>邀请人手机号</label></p><input type={'text'} onChange={(e)=>{this.setState({inviteName:e.target.value})}}/></div>
+                        <div className={style.add_invite} onClick={this.addInvite}>添加</div>
                     </div>
                     <div className={style.selected}>
                         <div>
                             <span className={style.FilletRectangle}></span><span>已选套餐</span>
                         </div>
                     </div>
-                    <ul className={style.list}>
-                        <li className={style.item}>
-                            <p>NTF 炫酷房车1-1</p>
-                            <p>总价：700000.00元</p>
-                            <p>单价：20000.00元</p>
-                        </li>
-                    </ul>
-                    <div className={style.package}>
-                        <div><span>共2个套餐包</span>支付订金：<span>¥7000.00元</span></div>
-                        <p>需支付尾款：<span>¥133000.00元</span></p>
-                    </div>
-                    <div className={style.pay}>
-                        <p>支付方式</p>
-                        <p><img src={wcpay} className={style.left_img} alt={"支付"}/>微信支付<span className={style.right_img}><img src={icon_cs} alt={""}/></span></p>
-                        <p className={style.agree}>
-                          本人已阅读并同意<span>《嘟嘟平台服务协议》</span></p>
-                        <div className={style.gopay}>去支付</div>
+                    <div className={style.list} >
+                        <ul className={style.listItem}>
+                        {
+                            orderList.map((item,index)=>{
+                                return(
+                                    <li
+                                        key={index}
+                                        className={style.item}>
+                                        <p><span>{item.crowdfundingName}</span><span>x1</span></p>
+                                        <p>总价：{item.crowdfundingPrice}.00元</p>
+                                        <p>单价：{item.crowdfundingPartPrice}.00元</p>
+                                    </li>
+                                )
+                            })
+                        }
+                        </ul>
+                        <div className={style.package}>
+                            <div><span>共{countAll}个套餐包</span><span>支付订金：<label>¥7000.00元</label></span></div>
+                            <p>需支付尾款：<label>¥133000.00元</label></p>
+                        </div>
+                        <div className={style.pay}>
+                            <p>支付方式</p>
+                            <p><span><img src={wcpay} alt={"支付"}/>微信支付</span><span><img src={icon_cs} alt={""}/></span></p>
+                            <p className={style.agree}><input type={"radio"} className={style.checkRadio} onChange={this.change.bind(this)}/>本人已阅读并同意<span>《嘟嘟平台服务协议》</span></p>
+                            <div className={style.gopay}  onClick={this.goPay}>去支付</div>
+                        </div>
                     </div>
                 </div>
-            </div>
+                <div style={{width:'100%',height:'12px',background:'#ccc'}}></div>
+            </Fragment>
         )
     }
 
--- src/screen/MyOrder.js
+++ src/screen/MyOrder.js
@@ -1,37 +1,63 @@
 import React from 'react';
+import {Fragment} from 'react';
 import Header from "../components/common/Header";
-import style from "../styles/myorder.css";
+import storage from "../util/setStorage";
+import style from "../styles/myorder.module.css";
 import shop_url from "../assets/image/shop.png";
 import picture_more from "../assets/image/picture_more.png"
 export default class MyOrder extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-
+            list:[]
         }
+        this.token=storage.getItem('token')
     }
     componentDidMount() {
-
+        window.axios({
+            url:window.API.Crowd_funding.crowd_funding_list+'?pageIndex='+1+'&pageSize='+12,
+            method: 'GET',
+            headers:{
+                'Authorization': this.token
+            }
+        }).then(res=>{
+           if(res.code===200){
+               console.log(res.data.list)
+               this.setState({
+                   list: res.data.list
+               })
+           }
+        }).catch(err=>{
+            console.log(err)
+        })
     }
 
     render() {
+        const {list} =this.state;
         return (
-            <div>
+            <Fragment>
                 <Header/>
-                    <ul className={style.container}>
-                        <li className={style.item}>
-                            <div className={style.image}>
-                                <img src={shop_url} alt={"商品"}/>
-                            </div>
-                            <div className={style.content}>
-                                <p>DrF-高型房车</p>
-                                <p>已认筹：4份</p>
-                                <p>单价：20000.00元／份</p>
-                            </div>
-                            <div className={style.right_price}>
-                                <p>¥80000.00</p>
-                            </div>
-                        </li>
+                <div className={style.container}>
+                    <ul>
+                        {
+                            list.map((item,index)=>{
+                               return (<li
+                               key={index}
+                               >
+                                    <div className={style.image}>
+                                        <img src={item.cover} alt={item}/>
+                                    </div>
+                                    <div className={style.content}>
+                                        <div>{item.name}</div>
+                                        <div>已认筹：{item.sell}份</div>
+                                        <div>单价：{item.crowdfundingPartPrice}元／份</div>
+                                    </div>
+                                    <div className={style.right_price}>
+                                        <p>¥{item.crowdfundingPrice}</p>
+                                    </div>
+                                </li>)
+                            })
+                        }
                     </ul>
                     <div className={style.total}>
                         <div className={style.image}>
@@ -40,12 +66,13 @@ export default class MyOrder extends React.PureComponent {
                         <div className={style.more}>
                             <img src={picture_more}  alt={"商品"}/>
                         </div>
-                        <div className={style.right_price}>
-                            <p>¥80000.00</p>
-                            <p className={style.number}>共8份</p>
+                        <div className={style.price}>
+                            <div>¥80000.00</div>
+                            <div className={style.number}>共8份</div>
                         </div>
+                    </div>
                 </div>
-            </div>
+            </Fragment>
         )
     }
 
